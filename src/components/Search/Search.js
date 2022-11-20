3bc1fb55f5afbc29d299d2d07202ba4afe2d82d4
hari1538radha <hari.r2020ece@sece.ac.in>
2022-11-20 11:26:20 +0530

    feat:search component completed

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getEducationInfo } from "../../Store/Slice/Slice";
-
+import search from "../Images/search.png"
 const Search = () => {
   const dispath = useDispatch();
   const handelclick = () => {
     dispath(getEducationInfo());
   };
   return (
-    <div>
-      <input type="text" placeholder="enter the loation"></input>
+    <div className="flex  mt-10 items-center justify-center relative"> 
+      <input className="w-4/5 h-9 rounded-xl text-center" type="text" placeholder="Enter the location"></input>
       <button onClick={handelclick} className="">
-        hello
+     <img className=" absolute  left-[87%] top-[2px] h-8" src={search}></img>
       </button>
     </div>
   );
