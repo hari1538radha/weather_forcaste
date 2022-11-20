f5bdda5b47234d97ae3f0f5294d5b94b57180151
hari1538radha <hari.r2020ece@sece.ac.in>
2022-11-20 10:57:21 +0530

    feat:navbar completed

--- src/App.js
+++ src/App.js
@@ -2,41 +2,40 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getEducationInfo } from "./Store/Slice/Slice";
+import Navbar from "./Navbar/Navbar"
 
 function App() {
   const dispath = useDispatch();
- const handelclick = () =>{
-  dispath(getEducationInfo())
- }
- const{ weatherData,Loading} = useSelector(state => state.educationinfo)
- console.log(weatherData)
- console.log(Loading)
+  const handelclick = () => {
+    dispath(getEducationInfo());
+  };
+  const { weatherData, Loading } = useSelector((state) => state.educationinfo);
+  console.log(weatherData);
+  console.log(Loading);
   return (
-    <div className="App">
-      Hi
+    <div className=" h-screen bg-gradient-to-br from-[#536976] to-[#292e49]">
+      <Navbar/>
+      
       <div>
-      <div>
-        {Loading === true && (
-        <div className="third-full-con-pro">
-         <p>nodatafound</p>
-         <img src="https://i.gifer.com/origin/1e/1eca394692a06b6bdb31383a2a1bcaf9_w200.gif"></img>
-          </div>
-          )
-      }
+        <div>
+          {Loading === true && (
+            <div className="third-full-con-pro">
+              <p>nodatafound</p>
+              <img src="https://i.gifer.com/origin/1e/1eca394692a06b6bdb31383a2a1bcaf9_w200.gif"></img>
+            </div>
+          )}
         </div>
         <div>
-        {Loading === false && (
-        <div className="third-full-con-pro">
-          {weatherData && 
-          <p>{weatherData.latitude}</p>
-          }
-          </div>
-          )
-      }
+          {Loading === false && (
+            <div className="third-full-con-pro">
+              {weatherData && <p>{weatherData.address}</p>}
+            </div>
+          )}
         </div>
-    
+        
       </div>
-      <button onClick={handelclick}>hello</button>
+      <input type="text" placeholder="enter the loation"></input>
+      <button onClick={handelclick} className="">hello</button>
     </div>
   );
 }
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Logo from "../Images/weather-app (2).png";
+import Menu from "../Images/weather-app.png";
+import Settings from "../Images/settings.png";
+import Location from "../Images/pin.png";
+import Notifiocation from "../Images/notification.png";
+const Navbar = () => {
+  return (
+    <div>
+      <div className=" border-b-[.9px] border-slate-400 flex items-center justify-between ">
+        <div className=" pl-10 flex items-center justify-center">
+        <p className="font-semibold text-transparent bg-clip-text bg-gradient-to-br from-yellow-500 to-sky-400 text-4xl">Weatherfy</p>
+        </div>
+        <div className="flex p-5 gap-x-10 pr-14">
+          <div className="flex gap-y-2 flex-col justify-center items-center">
+            <img className="w-8" src={Menu}></img>
+            <label className=" font-semibold text-transparent bg-clip-text bg-gradient-to-br from-yellow-500 to-sky-400">Menu</label>
+          </div>
+          <div className="flex gap-y-2  flex-col justify-center items-center">
+            <img src={Location} className="w-8"></img>
+            <label className="font-semibold text-transparent bg-clip-text bg-gradient-to-br from-yellow-500 to-sky-400">Location</label>
+          </div>
+          <div className="flex gap-y-2  flex-col justify-center items-center">
+            <img src={Settings} className="w-8"></img>
+            <label className="font-semibold text-transparent bg-clip-text bg-gradient-to-br from-yellow-500 to-sky-400">Settings</label>
+          </div>
+          <div className="flex gap-y-2  flex-col justify-center items-center">
+            <img className="w-8" src={Notifiocation}></img>
+            <label className="font-semibold text-transparent bg-clip-text bg-gradient-to-br from-yellow-500 to-sky-400">Notification</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
